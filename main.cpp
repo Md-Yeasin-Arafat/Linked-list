62798524daf8f94b5a7a1a1bbeaa70da2fe4dbcb
Muhammed Yeasin Yeasin <>
2021-04-25 18:11:30 +0000

    second_commit

--- main.cpp
+++ main.cpp
@@ -4,15 +4,18 @@ using namespace std;
 typedef struct student{
     int id;
     char name[50];
+    struct student *prev;
     struct student *next;
 }student;
 
 void create(student *head);
 void insert(student *head,student *item,student *locafter);
-void del(student *head,student *prev);
-void fcreate(student *head, FILE *in);
+student *del(student *head,student *prev);
+//void fcreate(student *head, FILE *in);
+student *fcreate(student *head,FILE *in);
 void display(student *head);
 void traverse(student *head);
+void dll_reverse(student *last);
 student *search(student *head,int id);
 student *search_prev(student *head, int id);
 
@@ -21,35 +24,55 @@ int main()
 {
     FILE *in;
     int id;
-    student *first,*loc,*item;
-
-    // in=fopen("\Users\Dell\OneDrive\one drive\OneDrive\Documents\dummy.txt","r");
+    student *first,*loc,*last,*item;
+    in=fopen("/Users/Dell/Desktop/success.txt","r");
     first=(student*) malloc(sizeof(student));
-    create(first);
-    traverse(first);
 
-    // fcreate(first,in);
-    // fclose(in);
+    first->prev=NULL;
+    first->next=NULL;
+    last=first;
+
+    //create(first);
+    last=fcreate(first,in);
+    //traverse(first);
+     //fcreate(first,in);
+     fclose(in);
+     traverse (first);
 
     //display(first);
-    
+    cout<<"Reverse List"<<endl;
+    dll_reverse(last);
+
+
     cout<<"enter an id to search : ";
     cin>>id;
     loc=search(first,id);
-    traverse(first);
+    cout<<"Data found at "<<loc<<endl;
+    //traverse(first);
     // cout<<loc;
 
     cout<<"enter an id and name to insert : ";
     item=(student*) malloc(sizeof(student));
     cin>>item->id>>item->name;
+
+
     insert(first,item,loc);
     traverse(first);
+    cout<<"Reverse List"<<endl;
+    dll_reverse (last);
+
 
     //cout<<"enter an id to find its previous location : ";
     // search_prev=(student*)malloc(sizeof(student));
     cout<<"Enter an id to remove from the list : ";
     cin>>id;
-    loc=search_prev(first,id);
+    loc=search(first,id);
+    first=del(first,loc);
+
+
+    traverse(first);
+    cout<<"Reverse List"<<endl;
+    dll_reverse(last);
     //cout<<loc<<endl;
     //traverse(first);
 
@@ -76,13 +99,13 @@ void create(student *head){
 
 }
 
-void fcreate(student *head, FILE *in){
-    student *block;
+student *fcreate(student *head, FILE *in){
+    student *block,*prev;
     int id;
     char name[50];
     block=head;
 
-     if(fscanf(in,"%d %s",&id,name)==2){
+     if(fscanf(in,"%d%s",&id,name)==2){
 
         block->id=id;
 
@@ -92,11 +115,11 @@ void fcreate(student *head, FILE *in){
 
 
     while(fscanf(in,"%d %s",&id,name)==2){
-
+        prev=block;
         block->next=(student *) malloc(sizeof(student));
 
         block=block->next;
-
+        block->prev=prev;
         block->id=id;
 
         strcpy(block->name,name);
@@ -104,23 +127,33 @@ void fcreate(student *head, FILE *in){
     }
 
     block->next=NULL;
-    // while(fscanf(in,"%d%s",&id,name)==2){
-    //     block->id=id;
-    //     strcpy(block->name,name);
-    //     block->next=block;
-    // }
-    // block->next=NULL;
+    return block;
+
 }
 
 // \Users\Dell\OneDrive\one drive\OneDrive\Desktop
 
 void insert(student *head,student *item, student *locafter){
     item->next=locafter->next;
+    locafter->next->prev=item;
+
     locafter->next=item;
+    item->prev=locafter;
 }
 
-void del(student *head,student *prev){
+student *del(student *head,student *loc){
+    student *prev,*first;
+
+    if(loc->prev==NULL){
+        loc->next->prev==NULL;
+        first=loc->next;
+        delete loc;
+        return first;
+    }
+    prev=loc->prev;
     prev->next=prev->next->next;
+    delete loc;
+    return head;
 }
 
 void traverse(student *head){
@@ -129,6 +162,12 @@ void traverse(student *head){
     traverse(head->next);
 }
 
+
+void dll_reverse(student *last){
+    if(last==NULL)return;
+    cout<<last->id<<"\t"<<last->name<<endl;
+    dll_reverse(last->prev);
+}
 
 
 void display(student *head){
@@ -201,7 +240,7 @@ student *search_prev(student *head, int id){
 //             create(head->next);
 //         }
 //         else head->next=NULL;
-    
+
 // }
 
 
